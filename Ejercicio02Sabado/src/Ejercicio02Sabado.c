7152c458e863b4121eeca59370b7f4a2f66cab60
LeandroMS25 <leandro.m.sobrino@gmail.com>
2020-08-29 12:18:21 -0300

    Resolución ejercicio 2 Sabado

--- Ejercicio02Sabado/src/Ejercicio02Sabado.c
+++ Ejercicio02Sabado/src/Ejercicio02Sabado.c
@@ -7,7 +7,7 @@ Al ejercicio anterior :
 Ademas de la edad ingresar el estado civil de la persona ( 's' para soltera , 'c' para casada, 'd' divorciada)
 ademas de informar lo anterior ....informar:
 la persona mas joven de las casadas
-a persona mas Vieja de las solteras
+la persona mas Vieja de las solteras
 de los estados civiles , cual fue el mas ingresado
  ============================================================================
  */
@@ -16,36 +16,67 @@ de los estados civiles , cual fue el mas ingresado
 
 int main(void)
 {
-	char respuesta;
 	int edad;
-	int edadMaxima;
-	int edadMinima;
-	int contadorEdades=0;
-	int acumEdades=0;
-	float promedio;
+	int flagJovenCasado=0;
+	int edadJovenCasado;
+	int flagViejoSoltero=0;
+	int edadViejoCasado;
+	int contadorSolteros=0;
+	int contadorCasados=0;
+	int contadorDivorciados=0;
+	char estadoCivil;
+	char respuesta;
 
 	do
 	{
 		setbuf(stdout,NULL);
 		printf("Ingrese una edad: ");
 		scanf("%d",&edad);
-		if(contadorEdades == 0 || edad>edadMaxima)
+		printf("Ingrese el estado civil:\n(s para soltero - c para casado - d para divorciado): ");
+		fflush(stdin);
+		scanf("%c",&estadoCivil);
+		switch (estadoCivil)
 		{
-			edadMaxima = edad;
+			case 'c':
+				contadorCasados++;
+				if(flagJovenCasado == 0 || edad<edadJovenCasado)
+				{
+					edadJovenCasado = edad;
+					flagJovenCasado = 1;
+				}
+				break;
+			case 'd':
+				contadorDivorciados++;
+				break;
+			case 's':
+				contadorSolteros++;
+				if(flagViejoSoltero == 0 || edad>edadViejoCasado)
+				{
+					edadViejoCasado = edad;
+					flagViejoSoltero = 1;
+				}
+				break;
 		}
-		if(contadorEdades == 0 || edad<edadMinima)
-		{
-			edadMinima = edad;
-		}
-		contadorEdades++;
-		acumEdades+=edad;
 		printf("¿Desea ingresar otra edad? s/n: ");
 		fflush(stdin);
 		scanf("%c",&respuesta);
 	}while(respuesta == 's');
-	promedio = (float)acumEdades / contadorEdades;
 
-	printf("\nEl promedio de las edades ingresadas es: %.2f", promedio);
-	printf("\nLa edad maxima ingresada es: %d", edadMaxima);
-	printf("\nLa edad minima ingresada es: %d", edadMinima);
+	printf("\nLa persona mas joven casada tiene %d años",edadJovenCasado);
+	printf("\nLa persona mas vieja soltera tiene %d años",edadViejoCasado);
+	if(contadorCasados>contadorSolteros && contadorCasados>contadorDivorciados)
+	{
+		printf("\nEl estado civil mas ingresado es: Casados");
+	}
+	else
+	{
+		if(contadorDivorciados>contadorSolteros)
+		{
+			printf("\nEl estado civil mas ingresado es: Divorciados");
+		}
+		else
+		{
+			printf("\nEl estado civil mas ingresado es: Solteros");
+		}
+	}
 }
