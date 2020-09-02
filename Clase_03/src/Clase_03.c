ae5ad9d84d07e2dffd8ef87a67818a6b2730240b
LeandroMS25 <leandro.m.sobrino@gmail.com>
2020-09-01 21:17:18 -0300

    Clase 3 Funciones y punteros

--- Clase_03/src/Clase_03.c
+++ Clase_03/src/Clase_03.c
@@ -1,16 +1,57 @@
-/*
- ============================================================================
- Name        : Clase_03.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 #include <stdio.h>
 #include <stdlib.h>
 
+int sumarEnteros(int operadorA, int operadorB);
+int dividirEnteros(int operadorA, int operadorB, float *resultado);
+/*
+int sumar(int a, int b, int *resultado); Prueba 2
+*/
 int main(void)
 {
-
+	float resultadoDiv;
+	if(dividirEnteros(14,0,&resultadoDiv) == 0)
+	{
+		printf("El resultado es: %.2f",resultadoDiv); // Se usa en despues con &.
+	}
+	else
+	{
+		printf("No se puede dividir por 0.");
+	}
+	return EXIT_SUCCESS;
 }
+
+int sumarEnteros(int operadorA, int operadorB)
+{
+	int resultado;
+	resultado = operadorA + operadorB;
+	return resultado;
+}
+
+int dividirEnteros(int operadorA, int operadorB, float *resultadoDiv)
+{
+	int retorno;
+	if(operadorB != 0)
+	{
+		*resultadoDiv = (float)operadorA / operadorB; // Se declara en la funcion con asterisco.
+		retorno = 0;
+	}
+	else
+	{
+		retorno = -1;
+	}
+	return retorno;
+}
+
+/*	Prueba 2
+int main(void)
+{
+	int numero = 22;
+	sumar(14,11,&numero);
+}
+
+int sumar(int a, int b, int *resultado)
+{
+	*resultado = a + b;
+	return 0;
+}
+*/
